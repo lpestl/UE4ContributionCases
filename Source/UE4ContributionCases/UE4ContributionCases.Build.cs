10b5d6803a5ef034bbc2d790478aa22face17df9
Mikhail Kataev <m_kataev@wargaming.net>
2020-09-29 09:34:12 +0300

    Added example content

--- Source/UE4ContributionCases/UE4ContributionCases.Build.cs
+++ Source/UE4ContributionCases/UE4ContributionCases.Build.cs
@@ -4,20 +4,29 @@ using UnrealBuildTool;
 
 public class UE4ContributionCases : ModuleRules
 {
-	public UE4ContributionCases(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+    public UE4ContributionCases(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core", 
+            "CoreUObject", 
+            "Engine", 
+            "UnrealEd",
+            "InputCore",
+            "ClassViewer",
+            "DesktopPlatform"
+        });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PrivateDependencyModuleNames.AddRange(new string[] { });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        PrivateDependencyModuleNames.AddRange(new string[] {"Json", "JsonUtilities",});
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
